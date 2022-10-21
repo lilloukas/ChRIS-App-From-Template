483b7ece2e1b405868cd8afb4fd8ea79ff766d48
lilloukas <colejh@bu.edu>
2022-10-20 21:48:12 -0400

    random card as app

--- app.py
+++ app.py
@@ -3,7 +3,9 @@
 from pathlib import Path
 from argparse import ArgumentParser, Namespace, ArgumentDefaultsHelpFormatter
 from importlib.metadata import Distribution
-
+import requests
+import json
+from os.path import exists
 from chris_plugin import chris_plugin
 
 __pkg = Distribution.from_name(__package__)
@@ -11,7 +13,7 @@ __version__ = __pkg.version
 
 
 DISPLAY_TITLE = r"""
-ChRIS Plugin Template Title
+Random Cards
 """
 
 
@@ -26,13 +28,13 @@ parser.add_argument('-V', '--version', action='version',
 # documentation: https://fnndsc.github.io/chris_plugin/chris_plugin.html#chris_plugin
 @chris_plugin(
     parser=parser,
-    title='My ChRIS plugin',
+    title='Test ChRIS Plugin',
     category='',                 # ref. https://chrisstore.co/plugins
     min_memory_limit='100Mi',    # supported units: Mi, Gi
     min_cpu_limit='1000m',       # millicores, e.g. "1000m" = 1 CPU core
     min_gpu_limit=0              # set min_gpu_limit=1 to enable GPU
 )
-def main(options: Namespace, inputdir: Path, outputdir: Path):
+def main(options: Namespace, outputdir: Path):
     """
     :param options: non-positional arguments parsed by the parser given to @chris_plugin
     :param inputdir: directory containing input files (read-only)
@@ -41,8 +43,21 @@ def main(options: Namespace, inputdir: Path, outputdir: Path):
 
     print(DISPLAY_TITLE)
 
-    output_file = outputdir / f'{options.name}.txt'
-    output_file.write_text('did nothing successfully!')
+    
+    if not exists(str(outputdir)+"/random_cards.json"):
+            
+        random_cards = []
+        for i in range(10):
+            random_card = requests.get('https://db.ygoprodeck.com/api/v7/randomcard.php')
+            random_cards.append(random_card.json())
+            
+        # Get each random card stored as a single line in the json file    
+        json_cards = "\n".join([json.dumps(random_card) for random_card in random_cards])
+
+        # Save the random card jsons to a file in the specified directory
+        with open(str(outputdir)+"/random_cards.json", "w") as f:
+            f.write(json_cards)
+
 
 
 if __name__ == '__main__':
